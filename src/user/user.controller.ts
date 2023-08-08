f11500b05153c012b819112c0a0a8ce52612efd7
thiagomoyses <thiagodasilvamoysesfr@gmail.com>
2023-08-08 19:02:37 +0200

    create service, module, dto and controller of user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 // import { AuthGuard } from '@nestjs/passport';
 // import { Request } from 'express';
 import { JwtGuard } from '../auth/guard/index';
 import { GetUser } from '../auth/decorator';
 import { User } from '@prisma/client';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+    constructor(private userService: UserService){}
     @Get('me')
     getMe(@GetUser() user: User){
         return user;
+    }
+
+    @Patch()
+    editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto){
+        return this.userService.editUser(userId, dto)
     }
 }
